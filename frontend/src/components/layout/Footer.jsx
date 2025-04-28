94e2fc6faaf86797fd8ef81c672782fe8ee3ae85
Mahedul-Karim <karimrupo21@gmail.com>
2025-04-28 16:57:32 +0600

    footer added

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Container from "../common/Container";
+import Logo from "../common/Logo";
 
 const Footer = () => {
   return (
     <footer className="bg-background dark:bg-paper border-t border-solid border-border">
-      <Container className="py-[45px] md:py-16">This is footer</Container>
+      <Container className="py-8">
+        <div className="flex items-center justify-center">
+          <Logo />
+        </div>
+        <hr className="my-4 border-border border-dashed" />
+        <p className="text-center font-semibold text-secondary text-xs" >&copy; {new Date().getFullYear()}, CopyForge</p>
+      </Container>
     </footer>
   );
 };
--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-
-
 import Container from "../common/Container";
 import Logo from "../common/Logo";
 import NavActions from "../nav/NavActions";
@@ -9,10 +7,9 @@ import Nav from "../nav/Nav";
 import MobileNav from "../nav/MobileNav";
 
 const Header = () => {
-
   return (
     <>
-      <header className="border-b border-solid border-border bg-background dark:bg-paper py-2 sticky">
+      <header className="border-b border-solid border-border bg-background dark:bg-paper py-2 sticky top-0 z-[2]">
         <Container className="flex items-center justify-between">
           <div className="flex items-center">
             <Logo />
