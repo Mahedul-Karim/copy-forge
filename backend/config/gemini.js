d426cabeb9aa633d476f26b4144c815dc33bd9c7
Mahedul-Karim <karimrupo21@gmail.com>
2026-04-03 17:29:41 +0600

    gemini version updated

--- backend/config/gemini.js
+++ backend/config/gemini.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export const initializeAi = async () => {
   const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
   const model = genAi.getGenerativeModel({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
   });
 
   return model;
